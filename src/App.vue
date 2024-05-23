27fa386e19f01a47860aa8dfa7a03aa99d5187ae
Feliu <felix1991palm@gmail.com>
2024-05-23 21:54:12 +0200

    Basic setup and first Campaign component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,28 @@
-<script setup></script>
+<script setup>
+import { computed } from 'vue'
+import { useCampaignStore } from './stores/campaigns.js'
+import Campaign from './components/campaigns/Campaign.vue'
+
+const campaignStore = useCampaignStore()
+
+const campaigns = computed(() => campaignStore.getCampaigns)
+</script>
 
 <template>
-  <h1>Campaign Manager</h1>
+    <header class="header">
+        <h1>Marketing Campaign Manager</h1>
+    </header>
+
+    <main class="main">
+        <aside class="navigation"></aside>
+        <div class="main-content">
+            <div class="campaigns">
+                <Campaign v-for="campaign in campaigns" :campaign="campaign" :key="campaign.id" />
+            </div>
+        </div>
+    </main>
+
+    <footer class="footer"></footer>
 </template>
 
 <style scoped></style>
---
+++ src/components/campaigns/Campaign.vue
@@ -0,0 +1,25 @@
+<script setup>
+const props = defineProps({
+    campaign: Object,
+})
+</script>
+
+<template>
+    <div v-show="campaign.active" class="campaign">
+        <header class="campaign-header">
+            <h4>{{ campaign.name }}</h4>
+            <p>Client 1</p>
+            <div class="campaign-details">
+                <div class="campaign-detail">{{ campaign.platform }}</div>
+                <div class="campaign-detail">{{ campaign.type }}</div>
+            </div>
+        </header>
+        <main class="campaign-main">
+            <div class="campaign-info">{{ campaign.curreny }} {{ campaign.budget }}</div>
+        </main>
+        <footer class="campaign-footer">
+            <button class="campaign-button">Edit</button>
+            <button class="campaign-button">Archive</button>
+        </footer>
+    </div>
+</template>
